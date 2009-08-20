24ce19ca988e752cd2acc5d22c0548a5ddbb0f5e
willian <willian@willianfernandes.com.br>
2009-08-20 07:38:08 -0700

    Added a css class on body tag with the name of the browser

--- rails.js
+++ rails.js
@@ -9,13 +9,13 @@
 Usage: Add this code in your application.html.erb layout file
 
 <html>
-   <head>
-       <meta name="rails-controller" content="<%= response.controller_name %>" />
-       <meta name="rails-action" content="<%= response.action_name %>" />
-       <%= javascript_include_tag 'jquery' %>
-       <%= javascript_include_tag 'rails' %>
-       <%= javascript_include_tag 'lib.js' %>
-   </head>
+	<head>
+		<meta name="rails-controller" content="<%= response.controller_name %>" />
+		<meta name="rails-action" content="<%= response.action_name %>" />
+		<%= javascript_include_tag 'jquery' %>
+		<%= javascript_include_tag 'rails' %>
+		<%= javascript_include_tag 'lib.js' %>
+	</head>
 </html>
 
 Your lib.js should follow this rule:
@@ -25,22 +25,44 @@ Rails.users = {};
 Rails["index"] = function() {
 	// execute specific code for users/index
 };
-    
+	
 Rails["new"] = function() {
 	// execute specific code for users/new
 }
 */ 
 var Rails = {
-    ALIASES: {
-        'create': 'new',
-        'update': 'edit'
-    },
-    
-    dispatcher: function() {
-        var controller_name = $('head meta[name=rails-controller]').attr('content');
-        var action_name = $('head meta[name=rails-action]').attr('content');
-        
-        action_name = Rails.ALIASES[action_name] || action_name;
+	ALIASES: {
+		'create': 'new',
+		'update': 'edit'
+	},
+	
+	browserName: function() {
+		var css_name = null;
+		var ua = navigator.userAgent;
+		var matches = null;
+
+		if (ua.match(/firefox/i)) {
+			css_name = 'firefox';
+		} else if (ua.match(/safari/i)) {
+			css_name = 'safari';
+		} else if (matches = ua.match(/msie (\d+)/i)) {
+			css_name = 'ie ie' + matches[1];
+		} else if (ua.match(/opera/i)) {
+			css_name = 'opera';
+		} else if (ua.match(/mozilla/)) {
+			css_name = 'mozilla';
+		}
+
+		if (css_name) {
+			$('body').addClass(css_name);
+		}
+	},
+	
+	dispatcher: function() {
+		var controller_name = $('head meta[name=rails-controller]').attr('content');
+		var action_name = $('head meta[name=rails-action]').attr('content');
+		
+		action_name = Rails.ALIASES[action_name] || action_name;
 		
 		// Executed before every controller action
 		if (Rails.before) {
@@ -58,7 +80,8 @@ var Rails = {
 				Rails[controller_name][action_name]();
 			}
 		}
-    }
+		Rails.browserName();
+	}
 };
 
 $(document).ready(Rails.dispatcher);
