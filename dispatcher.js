49861e6c25debb13d5c6ce7fdcaf76cd79ab7439
Nando Vieira <fnando.vieira@gmail.com>
2012-03-29 16:09:40 -0300

    No more browser sniffing; If you need the old functionality, please get some older revision. Also removed the jQuery.stopEvent shortcut.

--- dispatcher.js
+++ dispatcher.js
@@ -1,84 +1,21 @@
 // Dispatcher.js - jQuery Dispatcher
-// Copyright (c) 2008-2011 Nando Vieira (nandovieira.com.br)
+// Copyright (c) 2008-2012 Nando Vieira (nandovieira.com.br)
 // Dual licensed under the MIT (MIT-LICENSE.txt)
 // and GPL (GPL-LICENSE.txt) license
 
-if (!window.App) {
-  var App = {};
-}
+;(function($){
+  this.App = this.App || {};
 
-var Dispatcher = {
-  ALIASES: {
+  var Dispatcher;
+  Dispatcher = this.Dispatcher = {};
+
+  Dispatcher.ALIASES = {
     "create": "new",
     "update": "edit",
     "destroy": "remove"
-  },
+  };
 
-  ua: navigator.userAgent,
-
-  init: function() {
-    Dispatcher.run();
-    Dispatcher.setBrowserInfo();
-  },
-
-  detectIE: function(minimumVersion, callback) {
-    var matches = this.ua.match(/MSIE (\d+)/)
-      , version = parseInt(matches[1], 10)
-    ;
-
-    if (!matches) { return; }
-
-    if (version < minimumVersion) {
-      callback(version, minimumVersion);
-    };
-  },
-
-  setBrowserInfo: function() {
-    $("body")
-      .removeClass("no-js")
-      .addClass("js")
-      .addClass(this.browserInfo().join(" "))
-    ;
-  },
-
-  browserInfo: function() {
-    var meta = ["non-ie"]
-      , matches = null
-    ;
-
-    if (this.ua.match(/firefox/i)) {
-      meta.push("firefox");
-    } else if (this.ua.match(/chrome/i)) {
-      $.merge(meta, ["chrome", "webkit"]);
-    } else if (this.ua.match(/iphone/i)) {
-      $.merge(meta, ["safari", "iphone", "webkit"]);
-    } else if (this.ua.match(/ipad/i)) {
-      $.merge(meta, ["safari", "ipad", "webkit"]);
-    } else if (this.ua.match(/safari/i)) {
-      $.merge(meta, ["safari", "webkit"]);
-    } else if ((matches = this.ua.match(/MSIE (\d+)/))) {
-      var version = parseInt(matches[1], 10);
-
-      meta.shift();
-      $.merge(meta, ["ie", "ie" + version]);
-
-      if (version < 9) {
-        meta.push("lt-ie9");
-      }
-
-      if (version < 8) {
-        meta.push("lt-ie8");
-      }
-    } else if (this.ua.match(/opera/i)) {
-      meta.push("opera");
-    } else if (this.ua.match(/mozilla/i)) {
-      meta.push("mozilla");
-    }
-
-    return meta;
-  },
-
-  run: function() {
+  Dispatcher.run = function() {
     var meta = $("head meta[name=page]")
       , noMeta = 'No meta tag found. Use something like <meta name="page" content="controller#action" />'
     ;
@@ -101,32 +38,17 @@ var Dispatcher = {
 
     if (App[controllerName]) {
       // Executed before any action from the current controller
-      if (App[controllerName].before) {
-        App[controllerName].before();
-      }
+      App[controllerName].before && App[controllerName].before();
 
       // The current action per-se
-      if (App[controllerName][actionName]) {
-        App[controllerName][actionName]();
-      }
+      App[controllerName][actionName] && App[controllerName][actionName]();
 
       // The after callback for the current controller
-      if (App[controllerName].after) {
-        App[controllerName].after();
-      }
+      App[controllerName].after && App[controllerName].after();
     }
 
-    if (App.after) {
-      App.after();
-    }
-  }
-};
-
-(function($){
-  $.stopEvent = function(e) {
-    e.stopPropagation();
-    e.preventDefault();
+    App.after && App.after();
   };
 
-  $(document).ready(Dispatcher.init);
-})(jQuery);
+  $(document).ready(Dispatcher.run);
+}).call(this, jQuery);
--- test/dispatcher_test.js
+++ test/dispatcher_test.js
@@ -1,15 +1,13 @@
-function prepare(ua) {
+function prepare() {
   $("meta[name=page]").remove();
   $("head").append('<meta name="page" content="" />');
-  $("body").removeAttr("class");
-  Dispatcher.ua = ua;
-  Dispatcher.init();
+  Dispatcher.run();
   App.sample = {};
 };
 
 new Test.Unit.Runner({
   setup: function() {
-    prepare(navigator.userAgent);
+    prepare();
     $("meta[name=page]").attr("content", "");
     App.sample = {};
     Dispatcher.before = null;
@@ -17,163 +15,6 @@ new Test.Unit.Runner({
 
   teardown: function() {
   },
-
-  // Return browser info
-  testReturnBrowserInfo: function() { with(this) {
-    prepare("iPad");
-    var meta = Dispatcher.browserInfo();
-
-    assertEqual(4, meta.length);
-    assertEqual("non-ie", meta[0]);
-    assertEqual("safari", meta[1]);
-    assertEqual("ipad", meta[2]);
-    assertEqual("webkit", meta[3]);
-  }},
-
-  // Detect Firefox
-  testDetectFirefox: function() { with(this) {
-    prepare("Firefox");
-    assertEqual(1, $("body.firefox").length);
-    assertEqual(1, $("body.non-ie").length);
-    assertEqual(1, $("body.js").length);
-  }},
-
-  // Detect Safari
-  testDetectSafari: function() { with(this) {
-    prepare("Safari");
-    assertEqual(1, $("body.safari").length);
-    assertEqual(1, $("body.webkit").length);
-    assertEqual(1, $("body.non-ie").length);
-    assertEqual(1, $("body.js").length);
-  }},
-
-  // Detect iPhone
-  testDetectiPhone: function() { with(this) {
-    prepare("iPhone");
-    assertEqual(1, $("body.iphone").length);
-    assertEqual(1, $("body.safari").length);
-    assertEqual(1, $("body.webkit").length);
-    assertEqual(1, $("body.non-ie").length);
-    assertEqual(1, $("body.js").length);
-  }},
-
-  // Detect iPad
-  testDetectiPad: function() { with(this) {
-    prepare("iPad");
-    assertEqual(1, $("body.ipad").length);
-    assertEqual(1, $("body.safari").length);
-    assertEqual(1, $("body.webkit").length);
-    assertEqual(1, $("body.non-ie").length);
-    assertEqual(1, $("body.js").length);
-  }},
-
-  // Detect Chrome
-  testDetectChrome: function() { with(this) {
-    prepare("Chrome");
-    assertEqual(1, $("body.chrome").length);
-    assertEqual(1, $("body.webkit").length);
-    assertEqual(1, $("body.non-ie").length);
-    assertEqual(1, $("body.js").length);
-  }},
-
-  // Detect Opera
-  testDetectOpera: function() { with(this) {
-    prepare("Opera");
-    assertEqual(1, $("body.opera").length);
-    assertEqual(1, $("body.non-ie").length);
-    assertEqual(1, $("body.js").length);
-  }},
-
-  // Detect IE6
-  testDetectIE6: function() { with(this) {
-    prepare("MSIE 6.0");
-    assertEqual(1, $("body.ie").length);
-    assertEqual(1, $("body.ie6").length);
-    assertEqual(0, $("body.non-ie").length);
-    assertEqual(1, $("body.js").length);
-    assertEqual(1, $("body.lt-ie9").length);
-    assertEqual(1, $("body.lt-ie8").length);
-  }},
-
-  // Detect IE7
-  testDetectIE7: function() { with(this) {
-    prepare("MSIE 7.0");
-    assertEqual(1, $("body.ie").length);
-    assertEqual(1, $("body.ie7").length);
-    assertEqual(0, $("body.non-ie").length);
-    assertEqual(1, $("body.js").length);
-    assertEqual(1, $("body.lt-ie9").length);
-    assertEqual(1, $("body.lt-ie8").length);
-  }},
-
-  // Detect IE8
-  testDetectIE8: function() { with(this) {
-    prepare("MSIE 8.0");
-    assertEqual(1, $("body.ie").length);
-    assertEqual(1, $("body.ie8").length);
-    assertEqual(0, $("body.non-ie").length);
-    assertEqual(1, $("body.js").length);
-    assertEqual(1, $("body.lt-ie9").length);
-    assertEqual(0, $("body.lt-ie8").length);
-  }},
-
-  // Detect IE9
-  testDetectIE9: function() { with(this) {
-    prepare("MSIE 9.0");
-    assertEqual(1, $("body.ie").length);
-    assertEqual(1, $("body.ie9").length);
-    assertEqual(0, $("body.non-ie").length);
-    assertEqual(1, $("body.js").length);
-    assertEqual(0, $("body.lt-ie9").length);
-    assertEqual(0, $("body.lt-ie8").length);
-  }},
-
-  // Detect Mozilla
-  testDetectMozilla: function() { with(this) {
-    prepare("Mozilla");
-    assertEqual(1, $("body.mozilla").length);
-    assertEqual(1, $("body.js").length);
-  }},
-
-  // Detect required IE version
-  testDetectRequiredIEVersion: function() { with(this) {
-    var args = {}
-      , handler = function(currentVersion, requiredVersion) {
-        args = {current: currentVersion, required: requiredVersion};
-      }
-    ;
-
-    prepare("MSIE 8.0");
-    Dispatcher.detectIE(8, handler);
-    assertEqual(undefined, args.current);
-    assertEqual(undefined, args.required);
-
-    prepare("MSIE 7.0");
-    Dispatcher.detectIE(8, handler);
-    assertEqual(7, args.current);
-    assertEqual(8, args.required);
-  }},
-
-  // Prevent event and propagation
-  testPreventEventAndPropagation: function() { with(this) {
-    var containerTriggered = null
-      , linkTriggered = null
-    ;
-
-    $("#sample-container").click(function(){
-      containerTriggered = true;
-    });
-
-    $("#sample-container a").click(function(e){
-      $.stopEvent(e);
-      linkTriggered = true;
-    });
-
-    $("#sample-container a").trigger("click");
-
-    assert(linkTriggered);
-    assertNull(containerTriggered);
-  }},
 
   // Dispatch before callback
   testDispatchBeforeCallback: function() { with(this) {
